95b04a3553e78d57333005383e0ab151db29e711
Nery2004 <josemolinatics@gmail.com>
2026-01-30 23:51:21 -0600

    Add initial implementation for data analysis and visualization of car losses

---
+++ Ejercicio3.r
@@ -0,0 +1,55 @@
+# Cargar datos
+carros <- read.csv("cars.csv", header = TRUE, stringsAsFactors = FALSE)
+
+# Crear data frame con promedios por marca
+peligrosos <- aggregate(
+  cbind(symboling, normalized_losses) ~ make,
+  data = carros,
+  FUN = mean,
+  na.rm = TRUE
+)
+
+# Ver las primeras 6 filas
+head(peligrosos)
+
+
+# EJERCICIO A
+
+# Cambiar nombres de columnas
+colnames(peligrosos) <- c(
+  "marca",
+  "indicadorPeligro",
+  "promedioPerdidas"
+)
+
+peligrosos
+
+# EJERCICIO B
+
+# Filtrar marcas sin NA en promedio de pérdidas
+peligrosos_sin_na <- subset(peligrosos, !is.na(promedioPerdidas))
+
+peligrosos_sin_na
+
+# EJERCICIO C
+# Vector de promedios de pérdidas
+vector_perdidas <- peligrosos_sin_na$promedioPerdidas
+
+# Nombres del vector = marcas
+names(vector_perdidas) <- peligrosos_sin_na$marca
+
+vector_perdidas
+
+# EJERCICIO D
+# Colores distintos para cada barra
+colores <- rainbow(length(vector_perdidas))
+
+# Gráfico de barras
+barplot(
+  vector_perdidas,
+  col = colores,
+  main = "Promedio de Pérdidas por Marca",
+  xlab = "Marca",
+  ylab = "Promedio de Pérdidas",
+  las = 2
+)
